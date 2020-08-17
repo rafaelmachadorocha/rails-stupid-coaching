c1e8f7d173178151afb7ced3de178a7f895e88b0
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-17 17:42:43 -0300

    adding files

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.match?(/\?/)
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    elsif @question.match?(/I'm going to work/)
+      @coach_answer = 'Great!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
